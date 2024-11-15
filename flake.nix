5cebf6e8a0315113473a8b1ada524bf11ec0a2e8
Sergei Stepanenko <kaptch@gmail.com>
2024-11-15 14:53:30 +0100

    packaging

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "category";
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-24.05;
     flake-utils.url = github:numtide/flake-utils;
   };
   outputs = { self, nixpkgs, flake-utils }:
@@ -11,24 +11,6 @@
         lib = pkgs.lib;
         coq = pkgs.coq_8_19;
         coqPkgs = pkgs.coqPackages_8_19;
-        # coq = pkgs.coq_8_20;
-        # coqPkgs = pkgs.coqPackages_8_20;
-        # ocamlPkgs = coq.ocamlPackages;
-        # stdpp-dev = coqPkgs.mkCoqDerivation
-        #   {
-        #     pname = "stdpp";
-        #     domain = "gitlab.mpi-sws.org";
-        #     owner = "iris";
-        #     defaultVersion = "1.10.0";
-        #     release."1.10.0".sha256 = "sha256-bfynevIKxAltvt76lsqVxBmifFkzEhyX8lRgTKxr21I=";
-        #     releaseRev = v: "coq-stdpp-${v}";
-        #     propagatedBuildInputs = [];
-        #     preBuild = ''
-        #       if [[ -f coq-lint.sh ]]
-        #       then patchShebangs coq-lint.sh
-        #       fi
-        #     '';
-        #   };
       in {
         packages = {
           coq-artifact = coqPkgs.mkCoqDerivation {
